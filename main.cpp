d82d2fc9c73a8a8f12b967b62bef721c1c33c338
Juan Martín Rivas <jmartinrivas14@gmail.com>
2023-07-23 12:20:39 -0300

    Creating isLoggedIn

--- main.cpp
+++ main.cpp
@@ -6,5 +6,24 @@ using namespace std;
 
 bool isLoggedIn()
 {
-    
+    string username, password, un, pw;
+
+    cout << "Enter your username: ";
+    cin >> username;
+    cout << "Enter your password: ";
+    cin >> password;
+
+    ifstream read("data\\" + username + ".txt");
+    getline(read, un);
+    getline(read, pw);
+
+    if (un == username && pw == password)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
 }
